49947eb72bb7f2f777928bfd7f6af56928e6492d
daniel-krausex <xtsjwm37041@outlook.com>
2025-11-27 19:54:16 +0800

    deploy

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -5,13 +5,13 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  const deployedFHECounter = await deploy("FHECounter", {
+  const deployedRift = await deploy("EncryptedRealityRift", {
     from: deployer,
     log: true,
   });
 
-  console.log(`FHECounter contract: `, deployedFHECounter.address);
+  console.log(`EncryptedRealityRift contract: `, deployedRift.address);
 };
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_encryptedRealityRift"; // id required to prevent reexecution
+func.tags = ["EncryptedRealityRift"];
